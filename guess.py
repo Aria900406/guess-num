5c145e5574478db62af252afa1c7b804d5862bcf
Aria Wang <aria900406@gmail.com>
2023-08-02 15:17:25 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,11 +1,16 @@
 import random
 
-r = random.randint(1, 100)
+start = input('請輸入隨機數字開始值: ')
+end = input('請輸入隨機數字結束值: ')
+start = int(start)
+end = int(end)
+
+r = random.randint(start, end)
 count = 0
 
 while True:
     count += 1
-    num = input('請猜一個介於1~100的數字: ')
+    num = input('請猜一個數字: ')
     num = int(num)
     if num == r:
         print('你猜對了!')
